92123e13e46b010e88cfb3f12a0d96d42b0be7a7
Jackson Huang <jacksonh201325@gmail.com>
2023-06-14 08:25:43 -0400

    Added google Omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get 'static_pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'static_pages#home'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
 end
