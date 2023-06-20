898839bbcdc9f149c515ec142490a816819a7f21
Jackson Huang <jacksonh201325@gmail.com>
2023-06-20 09:14:49 -0400

    Deleted console logs

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,7 +8,6 @@ window.Stimulus   = application
 
 // Create a event dispatcher for on load of google script
 window.initMap = () => {
-  console.log("Map initialized 2")
   const event = new Event("map-loaded")
   event.initEvent("map-loaded", true, true)
   window.dispatchEvent(event)
