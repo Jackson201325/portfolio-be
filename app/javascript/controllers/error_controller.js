0f3ebfc4a23ae01d50650915bdffaf10dff9462b
Jackson Huang <jacksonh201325@gmail.com>
2023-06-24 18:41:05 -0400

    feat: Add fancy login

---
+++ app/javascript/controllers/error_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["main"]
+
+  hideMessage() {
+    console.log("hideMessage")
+    console.log(this.mainTarget)
+
+    this.mainTarget.classList.add("opacity-0", "transition-opacity", "duration-500");
+
+    // Add a delay before applying the "hidden" class again
+    setTimeout(() => {
+      this.mainTarget.classList.add("hidden");
+    }, 500); // Adjust the delay duration as needed
+  }
+}
