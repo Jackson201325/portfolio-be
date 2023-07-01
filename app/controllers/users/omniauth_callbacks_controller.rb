8489eff764342f4acb17672fcb3c62e829e6d39d
Jackson Huang <jacksonhuang@Jacksons-MacBook-Pro.local>
2023-07-01 15:46:55 -0400

    feat: Added omniauth API login
    Api login using google but I have not yet tried it. Probably not working yet
     - Have not found a way to send the token I guess back to the front end

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,21 +3,7 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-developer-strategy
-
-    skip_before_action :verify_authenticity_token, only: %i[google_oauth2 doorkeeper]
-
-    def doorkeeper
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-
-      if @user.persisted?
-        @user.update_doorkeeper_credentials(request.env["omniauth.auth"])
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: "Doorkeeper") if is_navigational_format?
-      else
-        session["devise.doorkeeper_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
-    end
+    skip_before_action :verify_authenticity_token, only: %i[google_oauth2]
 
     def google_oauth2
       @user = create_from_omniauth
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,10 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  google_auth = Rails.application.credentials.google
-
-  google_client_id = google_auth[:client_id]
-  google_client_secret = google_auth[:client_secret]
-
-  provider :google_oauth2, google_client_id, google_client_secret, scope: "email"
-end
-OmniAuth.config.allowed_request_methods = %i[get]
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   google_auth = Rails.application.credentials.google
+#
+#   google_client_id = google_auth[:client_id]
+#   google_client_secret = google_auth[:client_secret]
+#
+#   provider :google_oauth2, google_client_id, google_client_secret
+# end
+#
+OmniAuth.config.allowed_request_methods = %i[get post]
