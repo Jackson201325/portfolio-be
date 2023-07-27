529d23b8f33bda3bc72514bb306d439f38a95e3f
Jackson Huang <jacksonh201325@gmail.com>
2023-07-27 07:15:16 -0400

    fix(reservation): Fix to reservation

--- app/jobs/event_job.rb
+++ app/jobs/event_job.rb
@@ -29,14 +29,14 @@ class EventJob < ApplicationJob
       checkout_session = event.data.object
       reservation = Reservation.find_by(session_id: checkout_session.id)
 
-      raise "Reservation not found for checkout session: #{checkout_session.id}" if reservation.nil?
+      raise "Reservation not found for checkout session ID: #{checkout_session.id}" if reservation.nil?
 
       reservation.update(status: :confirmed, stripe_payment_intent_id: checkout_session.payment_intent)
     when "charge.refunded"
       charge = event.data.object
       reservation = Reservation.find_by(stripe_payment_intent_id: charge.payment_intent)
 
-      raise "Reservation not found for checkout session: #{charge.payment_intent}" if reservation.nil?
+      raise "Reservation not found for Payment Intent ID: #{charge.payment_intent}" if reservation.nil?
 
       reservation.update(status: :cancelled)
     end
